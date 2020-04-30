b0da60cbf4e22c6f078e9080c4d35602c58f2d08
Alban Durrheimer <alban@knot.city>
2020-04-30 18:04:11 +0200

    Fix path, add missing commands and events types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,97 @@
 import axios = require('axios');
 import reqSigner = require('@knot/request-signer');
 
+export enum KnotTypeEvents
+{
+    Unlocked = 'unlocked',
+    Locked = 'locked',
+    Boot = 'boot',
+    State = 'state',
+    Shake = 'shake',
+    HighTemp = 'high-temp',
+    CriticalEnergy = 'critical-energy',
+    UnexpectedUnlocked = 'unexpected-unlock',
+    SpotDefect = 'spot-defect'
+}
+
+type EventBase = {
+    event: KnotTypeEvents,
+    station: number
+}
+
+export type UnlockedEvents = EventBase & {
+    event: KnotTypeEvents.Unlocked,
+    data: {
+        spot: number,
+        unlock: number
+    }
+};
+
+export type LockedEvents = {
+    event: KnotTypeEvents.Locked,
+    data: {
+        spot: number,
+        vehicle: number,
+        cache_accepted: boolean,
+        time: number
+    }
+};
+
+export type BootEvents = EventBase & {
+    event: KnotTypeEvents.Boot,
+};
+
+export type StateEvents = EventBase & {
+    event: KnotTypeEvents.State,
+    data: {
+        mainboard: number,
+        vehicles: number[]
+    }
+};
+
+export type ShakeEvents = EventBase & {
+    event: KnotTypeEvents.Shake,
+};
+
+export type HighTempEvents = EventBase & {
+    "event": KnotTypeEvents.HighTemp,
+    "data": {
+        "temperature": number,
+        "critical": boolean
+    }
+};
+
+export type CriticalEnergyEvents = EventBase & {
+    "event": KnotTypeEvents.CriticalEnergy,
+};
+
+export type UnexpectedUnlockedEvents = EventBase & {
+    "event": KnotTypeEvents.UnexpectedUnlocked,
+    "data": {
+        "spot": number
+    }
+};
+
+export type SpotDefectEvents = EventBase & {
+    "event": KnotTypeEvents.SpotDefect,
+    "data": {
+        "spot": number,
+        "vehicle": number,
+        "vehicle_voltage": number,
+        "lock_status": number
+    }
+};
+
+export type KnotEvents = UnlockedEvents | LockedEvents | BootEvents | StateEvents | ShakeEvents | HighTempEvents | CriticalEnergyEvents | UnexpectedUnlockedEvents | SpotDefectEvents;
+
+
 interface KnotSASOptions
 {
     endpoint?: string;
     privateKey: string;
     keyId: string;
 }
+
 
 export class KnotSAS
 {
@@ -45,11 +130,12 @@ export class KnotSAS
 
         this.#options = options;
         this.#ax = axios.default.create({
-            baseURL: this.#options.endpoint || 'https://services.knotcity.io'
+            baseURL: this.#options.endpoint || 'https://staas.knotcity.io'
         });
         this.#ax.interceptors.request.use(c =>
         {
             c.headers['X-Knot-Date'] = +new Date();
+            c.headers['X-Api-Key'] = this.#options.keyId;
             c.headers['Content-Type'] = 'application/json';
             c.headers['Content-Length'] = c.data ? JSON.stringify(c.data).length : 0;
             try
@@ -76,12 +162,12 @@ export class KnotSAS
 
     rebootStation(id: number)
     {
-        return this.makeStationRequest(id, 'reboot');
+        return this.makeStationRequest('v0.1', id, 'reboot');
     }
 
     pingStation(id: number)
     {
-        return this.makeStationRequest(id, 'ping');
+        return this.makeStationRequest('v0.1', id, 'ping');
     }
 
     unlockSpot(stationId: number, spotId: number, unlockId: number)
@@ -94,19 +180,31 @@ export class KnotSAS
         {
             throwError('Unlock ID should be an integer greater or equal to 0');
         }
-        return this.makeStationRequest(stationId, 'unlock', {
+        return this.makeStationRequest('v0.1', stationId, 'unlock', {
             spot: spotId,
             unlock: unlockId
         });
     }
 
-    private makeStationRequest(id: number, action: string, data?: any)
+    scanAllStationSpot(id: number)
+    {
+        return this.makeStationRequest('v0.1', id, 'refresh');
+    }
+
+    confirmLockSpot(stationId: number, spotId: number)
+    {
+        return this.makeStationRequest('v0.1', stationId, 'lock-response', {
+            spot: spotId
+        });
+    }
+
+    private makeStationRequest(version: string, id: number, action: string, data?: any)
     {
         if (!Number.isInteger(id) || id < 1)
         {
             throwError('Station ID should be an integer greater or equal to 1');
         }
-        return this.makeRequest(`/stations/${id}/${action}`, data);
+        return this.makeRequest(`/${version}/${id}/${action}`, data);
     }
 
     private async makeRequest(urn: string, data?: any)
